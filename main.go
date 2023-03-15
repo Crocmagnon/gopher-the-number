7efe5d8b9630c746d1cb10963358f5d0744a687c
Gabriel Augendre <gabriel@augendre.info>
2023-03-15 18:08:34 +0100

    Add a message at the beginning of the game

--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"os"
 )
 
 func main() {
-	random := rand.Intn(100)
+	max := 100
+	fmt.Printf("I've picked a number between 0 and %d. Can you guess it?\n", max)
+	random := rand.Intn(max)
 	LoopUntilFound(os.Stdout, os.Stdin, random)
 }
